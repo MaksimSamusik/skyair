c2cf55c103b4f71543966126454e7c4d10e09a64
givemebape <125454593+MaksimSamusik@users.noreply.github.com>
2024-12-23 16:34:33 +0300

    Update settings.py

--- clothes/clothes/settings.py
+++ clothes/clothes/settings.py
@@ -180,8 +180,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 465
-EMAIL_HOST_USER = "djangotesting1@yandex.ru"
-EMAIL_HOST_PASSWORD = "qeltyriirzggotqf"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_SSL = True
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
